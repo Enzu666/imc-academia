43f37c35136e558788db2c790302217267200b22
ENZO CREPALDI PEIXOTO <25203666@CFP127ALUNO.LOCAL>
2025-09-22 17:00:18 -0300

    criando app Imc para academia

--- src/br/com/senaijandira/imcacademia/ImcAcademia.java
+++ src/br/com/senaijandira/imcacademia/ImcAcademia.java
@@ -1,4 +1,27 @@
 package br.com.senaijandira.imcacademia;
+import java.util.Scanner;
 
 public class ImcAcademia {
+    String nomeDoAluno;
+    double pesoDoAluno;
+    double alturaDoAluno;
+    double imcAluno;
+
+    public void obterDadosAluno() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
+        nomeDoAluno = leitor.nextLine();
+
+        System.out.print("Digite o peso do aluno: ");
+        pesoDoAluno = leitor.nextDouble();
+
+        System.out.print("Digite a altura do aluno: ");
+        alturaDoAluno = leitor.nextDouble();
+        calcularImc();
+    }
+
+    public void calcularImc() {
+        imcAluno = pesoDoAluno / (alturaDoAluno * alturaDoAluno);
+        System.out.println(imcAluno);
+    }
 }
--- src/br/com/senaijandira/imcacademia/ImcAcademiaApp.java
+++ src/br/com/senaijandira/imcacademia/ImcAcademiaApp.java
@@ -1,4 +1,13 @@
 package br.com.senaijandira.imcacademia;
 
 public class ImcAcademiaApp {
+    public static void main(String[] args) {
+
+        System.out.println("Iniciando aplicativo");
+
+        ImcAcademia conversor = new ImcAcademia();
+
+        conversor.obterDadosAluno();
+
+            }
 }
